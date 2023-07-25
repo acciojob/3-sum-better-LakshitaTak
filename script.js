7a963a831b03ecfda43837847901c96622954b9e
LakshitaTak <134226199+LakshitaTak@users.noreply.github.com>
2023-07-25 21:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
- let arr = [-1, 2, 1, -4]
- let target = 1
+ // let arr = [-1, 2, 1, -4]
+ // let target = 1
  //let sum = [];
  let sum = 99999;
 
  // -4,-1,1,2
- arr = arr.sort((a, b) => {
-   return a - b
- })
 
  function threeSum(arr, target) {
    // write your code here
-   for (let i = 0; i < arr.length - 2; i++) {
+	 const sortedArr = arr.sort((a, b) => {
+   return a - b
+ })
+   for (let i = 0; i < sortedArr.length - 2; i++) {
 
      // 1. sum = -4-1+1 = -4
      //2. sum = -1+1+2 = 2
-     console.log(arr[i], arr[i + 1], arr[i + 2])
-     let smallestSum = Math.abs(arr[i] + arr[i + 1] + arr[i + 2] - target)
+     console.log(sortedArr[i], sortedArr[i + 1], sortedArr[i + 2])
+     let smallestSum = Math.abs(sortedArr[i] + sortedArr[i + 1] + sortedArr[i + 2] - target)
      if (sum > smallestSum) {
        sum = smallestSum
      }
