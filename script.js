2428dca165064cf511d647faac27be22b646c856
LakshitaTak <134226199+LakshitaTak@users.noreply.github.com>
2023-07-25 21:50:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
+ let arr = [-1, 2, 1, -4]
+ let target = 1
+ //let sum = [];
+ let sum = 99999;
 
-module.exports = threeSum;
+ // -4,-1,1,2
+ arr = arr.sort((a, b) => {
+   return a - b
+ })
+
+ function threeSum(arr, target) {
+   // write your code here
+   for (let i = 0; i < arr.length - 2; i++) {
+
+     // 1. sum = -4-1+1 = -4
+     //2. sum = -1+1+2 = 2
+     console.log(arr[i], arr[i + 1], arr[i + 2])
+     let smallestSum = Math.abs(arr[i] + arr[i + 1] + arr[i + 2] - target)
+     if (sum > smallestSum) {
+       sum = smallestSum
+     }
+     //sum.push(Math.abs(arr[i] + arr[i + 1] + arr[i + 2]- target))
+   }
+   return sum
+ }
+ threeSum(arr, target)
+ console.log(sum)
+
+ //threeSum(arr, target)
+ // module.exports = threeSum;
+ //console.log(sum, Math.min(...sum))
